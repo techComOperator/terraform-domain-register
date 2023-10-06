9e9723030f82b158abd6a636ab9707788113c0a2
Kenny Raghunath <Kenny.Raghunath@gmail.com>
2023-10-06 10:52:50 -0400

    Added root and alternative domain outputs.

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,12 @@ output "root_domain_deligation_set" {
     value = aws_route53_delegation_set.delset
 }
 
+output "root_domain_certificate" {
+    description = "Created domain certificate for the root."
+    value = aws_acm_certificate.domain_acm_certificate
+}
+
+output "alternative_domain_certifcates" {
+    description = "An array of objects that houses any alternative domains."
+    value = aws_acm_certificate.alternative_acm_certificates
+}
