f54ea4ab7d8fe2508ec232add40335ab121fa670
Kenny Raghunath <Kenny.Raghunath@gmail.com>
2023-10-07 23:18:59 -0400

    Added alt domain example.

--- test/examples/alternative_domains/main.tf
+++ test/examples/alternative_domains/main.tf
@@ -2,6 +2,8 @@ module "domain-register" {
   source  = "../../.."
 
 # If you're running the tests manually, please use your own domain that you own
-# in AWS. Also, no tags are involved.
+# in AWS.
   domain_name = "example.com"
+  tag_list = ["a tag"]
+  acm_alternative_domain_list = ["this.example.com", "another.example.com"]
 }
